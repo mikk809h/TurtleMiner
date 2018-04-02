efd82608b9b29a5d1689d3babe18cd564382e3e2
Mikkel Andersen <3685424+mikk809h@users.noreply.github.com>
2018-04-02 16:41:08 +0200

    Added Turtle Class TODO: Inventory Subclass

--- Turtle.lua
+++ Turtle.lua
@@ -1,6 +1,127 @@
 local Turtle = {}
 
-function
+local Config = Config or nil
+
+if not Config then
+    Config = loadfile(fs.combine(TurtleMiner.Path, "Config.lua"))()
+    Config:Init()
+end
+Config.Orientation = Config.Orientation or 0
+
+Config.Y = Config.Y or 0
+Config.X = Config.X or 0
+Config.Z = Config.Z or 0
+
+function Turtle:Move(arg, direction)
+    if not self then
+        self = Turtle
+    else
+        direction = arg
+    end
+
+    if direction == "up" then
+        while not turtle.up() do
+            turtle.digUp()
+            turtle.attackUp()
+            sleep(.5)
+        end
+        Config:Update("Y", Config.Y + 1)
+    elseif direction == "down" then
+        while not turtle.down() do
+            turtle.digDown()
+            turtle.attackDown()
+            sleep(.5)
+        end
+        Config:Update("Y", Config.Y - 1)
+    elseif direction == "north" then
+        self:Turn(direction)
+        while not turtle.forward() do
+            turtle.dig()
+            turtle.attack()
+            sleep(.5)
+        end
+        Config:Update("X", Config.X + 1)
+    elseif direction == "east" then
+        self:Turn(direction)
+        while not turtle.forward() do
+            turtle.dig()
+            turtle.attack()
+            sleep(.5)
+        end
+        Config:Update("Z", Config.Z + 1)
+    elseif direction == "south" then
+        self:Turn(direction)
+        while not turtle.forward() do
+            turtle.dig()
+            turtle.attack()
+            sleep(.5)
+        end
+        Config:Update("X", Config.X - 1)
+    elseif direction == "west" then
+        self:Turn(direction)
+        while not turtle.forward() do
+            turtle.dig()
+            turtle.attack()
+            sleep(.5)
+        end
+        Config:Update("Z", Config.Z - 1)
+    else
+        return false, "Direction " .. direction .. " invalid!"
+    end
+end
+
+function Turtle:Turn(arg, direction)
+    if not self then
+        self = Turtle
+    else
+        direction = arg
+    end
+
+    if direction == "north" then
+        if Config.Orientation > 0 and Config.Orientation <= 2 then
+            repeat
+                turtle.turnLeft()
+                Config:Update("Orientation", Config.Orientation - 1)
+            until Config.Orientation == 0
+        elseif Config.Orientation == 3 then
+            turtle.turnRight()
+            Config:Update("Orientation", 0)
+        end
+    elseif direction == "east" then
+        if Config.Orientation > 1 and Config.Orientation <= 3 then
+            repeat
+                turtle.turnLeft()
+                Config:Update("Orientation", Config.Orientation - 1)
+            until Config.Orientation == 1
+        elseif Config.Orientation == 0 then
+            turtle.turnRight()
+            Config:Update("Orientation", 1)
+        end
+    elseif direction == "south" then
+        if Config.Orientation >= 0 and Config.Orientation <= 1 then
+            repeat
+                turtle.turnRight()
+                Config:Update("Orientation", Config.Orientation + 1)
+            until Config.Orientation == 2
+        elseif Config.Orientation == 3 then
+            turtle.turnLeft()
+            Config:Update("Orientation", 2)
+        end
+
+    elseif direction == "west" then
+        if Config.Orientation > 0 and Config.Orientation <= 2 then
+            repeat
+                turtle.turnRight()
+                Config:Update("Orientation", Config.Orientation + 1)
+            until Config.Orientation == 3
+        elseif Config.Orientation == 0 then
+            turtle.turnLeft()
+            Config:Update("Orientation", 3)
+        end
+    else
+        return false, "Direction " .. direction .. " invalid!"
+    end
+end
 
 
 local mt = {}
